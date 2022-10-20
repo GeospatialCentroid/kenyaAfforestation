cc0f9da211221227e1bd3cad67147c7f232b66ba
Mothes <ccmothes@colostate.edu>
2022-10-20 14:00:20 -0600

    init homepage and css style docs

--- app.r
+++ app.r
@@ -18,62 +18,106 @@ panelNames <- c("Optimistic Climate Future", "Middle of the Road Climate Future"
 county_names<- c("one","two","three")
 # UI section --------------------------------------------------------------
 ui <- navbarPage(
-  theme = bs_theme(version = 5, bootswatch = "minty"),
+  theme = bs_theme(version = 5, bootswatch = "lumen") %>% 
+    bslib::bs_add_rules(sass::sass_file("www/style.scss")),
   # the text that appears next to the pages 
-  title =  "Kenya Afforestation Decision Support Tool",
+  title =  HTML("Kenya Afforestation </br> Decision Support Tool"),
   # the text in the browser tab 
   windowTitle = "Kenya Afforestation Application",
   # means of applying data to all pages -- could be useful in footer section
   # header = h5("This content appears at the top of every page "),
   # footer = "This content appears at the bottom of every page",
   
-  # need a landing page for the application 
-  tabPanel( title = "Home"
+# Home page --------------------------------------------------------------- 
+  tabPanel(title = "Home",
+           htmlTemplate("www/homepage.html",
+                        button = actionButton("action", "Action"))
             
             
   ),
 
-# Optimistic --------------------------------------------------------------
-  tabPanel( title = "Optimistic",
-            tabsetPanel(type = "pills",
-                                  tabPanel("Climate change in Kenya",
-                                           map_UI(id = "ssp126", panelName = panelNames[1])),
-                                  tabPanel("Climate and management effects on forests",
-                                            map2_UI(id = "ssp126_2", panelName = panelNames[1], county_names = county_names)))),
-# Middle of the road ------------------------------------------------------
-tabPanel( title = "Middle of the Road",
-          tabsetPanel(type = "pills",
-                      tabPanel("Climate change in Kenya",
-                               map_UI(id = "ssp245", panelName = panelNames[2])),
-                      tabPanel("Climate and management effects on forests",
-                               map2_UI(id = "ssp245_2", panelName = panelNames[2], county_names = county_names)))),
-# Pessimistic -------------------------------------------------------------
-tabPanel( title = "Pessimistic",
-          tabsetPanel(type = "pills",
-                      tabPanel("Climate change in Kenya",
-                               map_UI(id = "ssp370", panelName = panelNames[3])),
-                      tabPanel("Climate and management effects on forests",
-                               map2_UI(id = "ssp370_2", panelName = panelNames[3], county_names = county_names)))),
-# Extreme Heat ------------------------------------------------------------
-tabPanel( title = "Extreme Heat",
-          tabsetPanel(type = "pills",
-                      tabPanel("Climate change in Kenya",
-                               map_UI(id = "ssp585", panelName = panelNames[4])),
-                      tabPanel("Climate and management effects on forests",
-                               map2_UI(id = "ssp585_2", panelName = panelNames[4], county_names = county_names)))),
+# combine scenarios into navbar menu
+navbarMenu(
+  title = "Climate Scenarios",
+  # Optimistic --------------------------------------------------------------
+  tabPanel(title = "Optimistic",
+           tabsetPanel(
+             type = "pills",
+             tabPanel(
+               "Climate change in Kenya",
+               map_UI(id = "ssp126", panelName = panelNames[1])
+             ),
+             tabPanel(
+               "Climate and management effects on forests",
+               map2_UI(
+                 id = "ssp126_2",
+                 panelName = panelNames[1],
+                 county_names = county_names
+               )
+             )
+           )),
+  # Middle of the road ------------------------------------------------------
+  tabPanel(title = "Middle of the Road",
+           tabsetPanel(
+             type = "pills",
+             tabPanel(
+               "Climate change in Kenya",
+               map_UI(id = "ssp245", panelName = panelNames[2])
+             ),
+             tabPanel(
+               "Climate and management effects on forests",
+               map2_UI(
+                 id = "ssp245_2",
+                 panelName = panelNames[2],
+                 county_names = county_names
+               )
+             )
+           )),
+  # Pessimistic -------------------------------------------------------------
+  tabPanel(title = "Pessimistic",
+           tabsetPanel(
+             type = "pills",
+             tabPanel(
+               "Climate change in Kenya",
+               map_UI(id = "ssp370", panelName = panelNames[3])
+             ),
+             tabPanel(
+               "Climate and management effects on forests",
+               map2_UI(
+                 id = "ssp370_2",
+                 panelName = panelNames[3],
+                 county_names = county_names
+               )
+             )
+           )),
+  # Extreme Heat ------------------------------------------------------------
+  tabPanel(title = "Extreme Heat",
+           tabsetPanel(
+             type = "pills",
+             tabPanel(
+               "Climate change in Kenya",
+               map_UI(id = "ssp585", panelName = panelNames[4])
+             ),
+             tabPanel(
+               "Climate and management effects on forests",
+               map2_UI(
+                 id = "ssp585_2",
+                 panelName = panelNames[4],
+                 county_names = county_names
+               )
+             )
+           ))
+),
 
 # Additional Nav bar objects ----------------------------------------------
-  navbarMenu(title = "Additional Resources", 
-             tabPanel( title = "Downloads",
-                       h2("content will be added to Model Downloads")
-             ),
-             tabPanel(title = "Model Validation",
-                      h2("content will be added to Model Validation")
-             ),
-             tabPanel(title = "Simulation Details",
-                h2("content will be added to Simulation Details")      
-             )           
-  )
+
+tabPanel(title = "Downloads",
+         h2("content will be added to Model Downloads")),
+tabPanel(title = "Model Validation",
+         h2("content will be added to Model Validation")),
+tabPanel(title = "Simulation Details",
+         h2("content will be added to Simulation Details"))
+
 )
 
 server <- function(input,output, session){
