d0882fdb1e1075ac671c238ddde751d7b71649bf
Caitlin Mothes <ccmothes@gmail.com>
2023-12-06 11:08:09 -0700

    develop new workflow for data export

--- utilities/exportRasterData.R
+++ utilities/exportRasterData.R
@@ -1,28 +1,13 @@
 # script to export and organize rasters for Zenodo data publish
+# All functions for exporting are written first, implementation found in last section
 
-
-
-# testing  ----------------------------------------------------------------
-# file = "~/Documents/kenyaAfforestation/appData/climateChangeInputs.RDS"
-# # fivle = "~/Documents/kenyaAfforestation/appData/validationInputs_carbon.RDS" # stored as nested list 
-# exportPath = "exportData"
-# 
-# exportRaster(file = rdsFiles,
-#              exportPath = exportPath)
-# 
-
-
-#' Export rasters
-#'
+#' Export Climate Rasters
+#' @param file : path to rds file of climate rasters
 #' @param exportPath : folder location to save outputs 
-#' @param file : path to rds file of interest 
-#'
-#' @description
-#' grabs an RDS or list from the appDate file and writes about a series of .tifs that can be share on a non R environment
-#' 
-#' @export A series of tifs
+#' @description Pulls the rasters in the given .RDS files and writes a series of .tifs saved to specified subfolders
 exportClimateRasters <- function(file, exportPath) {
-  #argument to create export path if does not already exist
+  
+  # argument to create export path if does not already exist
   if (!dir.exists(exportPath)) {
     dir.create(exportPath)
   }
@@ -46,34 +31,28 @@ exportClimateRasters <- function(file, exportPath) {
        ~ if (!dir.exists(paste0(exportPath, "/", .x, "/Climate"))) {
          dir.create(paste0(exportPath, "/", .x, "/Climate"))
        })
-  
-  
-  
-  #read in .RDS file
+
+  # read in .RDS file
   x <- readRDS(file)
   
   # return all elements that are raster bricks
-  r1 <- x %>% keep( ~ is(.x, "RasterBrick"))
-  
-  # if empty, pluck from second level (for validation sets)
-  if (is_empty(r1)) {
-    r1 <-   pluck(x, 2) %>% keep( ~ is(.x, "RasterBrick"))
-  }
-  
+  r1 <- list_flatten(x) %>% keep( ~ is(.x, "RasterBrick"))
   
   # grab names for export file name reference
-  r1_names <- names(r1)
-  
+  n1 <- names(r1)
   
   #iterate over raster brick to save each layer with appropriate name
-  for (i in seq_along(r1_names)) {
-    print(paste0("export data from ", r1_names[i]))
+  for (i in seq_along(n1)) {
+    
+    print(paste("export", n1[i]))
+    
     # grab first element and all layer names
     r2 <- r1[[i]]
-    n1 <- names(r2)
+    n2 <- names(r2)
     
     #save each individual layer
-    for (j in seq_along(n1)) {
+    for (j in seq_along(n2)) {
+      
       # select first layer
       r3 <- r2[[j]]
       
@@ -85,16 +64,14 @@ exportClimateRasters <- function(file, exportPath) {
           r3,
           filename = paste0(
             exportPath,
-            "/",
-            "Historic/",
-            r1_names[i],
+            "/Historic/",
+            n1[i],
             "_",
             names(r3),
             ".tif"
           ),
           overwrite = TRUE
         )
-        
       }
       
       ## Optimistic
@@ -103,16 +80,14 @@ exportClimateRasters <- function(file, exportPath) {
           r3,
           filename = paste0(
             exportPath,
-            "/",
-            "Optimistic/Climate/",
-            r1_names[i],
+            "/Optimistic/Climate/",
+            n1[i],
             "_",
             names(r3),
             ".tif"
           ),
           overwrite = TRUE
         )
-        
       }
       
       ## Middle of the Road
@@ -121,16 +96,14 @@ exportClimateRasters <- function(file, exportPath) {
           r3,
           filename = paste0(
             exportPath,
-            "/",
-            "MiddleOfTheRoad/Climate/",
-            r1_names[i],
+            "/MiddleOfTheRoad/Climate/",
+            n1[i],
             "_",
             names(r3),
             ".tif"
           ),
           overwrite = TRUE
         )
-        
       }
       
       ## Pessimistic
@@ -139,16 +112,14 @@ exportClimateRasters <- function(file, exportPath) {
           r3,
           filename = paste0(
             exportPath,
-            "/",
-            "Pessimistic/Climate/",
-            r1_names[i],
+            "/Pessimistic/Climate/",
+            n1[i],
             "_",
             names(r3),
             ".tif"
           ),
           overwrite = TRUE
         )
-        
       }
       
       ## Extreme
@@ -157,50 +128,226 @@ exportClimateRasters <- function(file, exportPath) {
           r3,
           filename = paste0(
             exportPath,
-            "/",
-            "Extreme/Climate/",
-            r1_names[i],
+            "/Extreme/Climate/",
+            n1[i],
             "_",
             names(r3),
             ".tif"
           ),
           overwrite = TRUE
         )
-        
+      }
+    }
+  }
+}
+
+
+#' Export Disturbance Rasters
+#' @param file : path to rds file of disturbance/management rasters
+#' @param exportPath : folder location to save outputs 
+#' @description Pulls the rasters in the given .RDS files and writes a series of .tifs saved to specified subfolders
+exportDisturbanceRasters <- function(file, exportPath) {
+  
+  #argument to create export path if does not already exist
+  if (!dir.exists(exportPath)) {
+    dir.create(exportPath)
+  }
+  
+  # create sub folders
+  
+  ## strings of folder names
+  sub <-
+    c("Historic",
+      "Optimistic",
+      "MiddleOfTheRoad",
+      "Pessimistic",
+      "Extreme")
+  
+  walk(sub,
+       ~ if (!dir.exists(paste0(exportPath, "/", .x))) {
+         dir.create(paste0(exportPath, "/", .x))
+       })
+  
+  walk(sub[-1],
+       ~ if (!dir.exists(paste0(exportPath, "/", .x, "/Disturbance"))) {
+         dir.create(paste0(exportPath, "/", .x, "/Disturbance"))
+       })
+  
+  #read in .RDS file
+  x <- readRDS(file)
+  
+  # return all elements that are raster layers (historic and expanding tree cover)
+  r_hist <- list_flatten(x) %>% keep( ~ is(.x, "RasterLayer"))
+  
+  # write to Historic? folder
+  writeRaster(r_hist$expandedForest,
+              filename = paste0(exportPath, "/Historic/Baseline_TreeCover.tif"))
+  
+  writeRaster(r_hist$existingForest,
+              filename = paste0(exportPath, "/Historic/Historic_TreeCover.tif"))
+  
+  # return all raster bricks of change rasters
+  r_change <- list_flatten(x) %>% keep( ~ is(.x, "RasterBrick"))
+  
+  # grab names to iterate over
+  n1 <- names(r_change)
+  
+  #iterate over raster brick to save each layer with appropriate name
+  for (i in seq_along(n1)) {
+    
+    print(paste("export",n1[i]))
+    
+    #grab first element and all layer names
+    r2 <- r_change[[i]]
+    n2 <- names(r2)
+    
+    #save each individual layer
+    for (j in seq_along(n2)) {
+      
+      #select layer
+      r3 <- r2[[j]]
+      
+      #change file names to be more accurate
+      if (str_detect(names(r3), "nothing")) {
+        n3 <- str_replace(names(r3), "donothing",  "currentFireSeverity")
+      } else if (str_detect(names(r3), "stopfires")) {
+        n3 <- str_replace(names(r3), "stopfires",  "decreaseFireSeverity")
+      } else if (str_detect(names(r3), "doublefires")) {
+        n3 <- str_replace(names(r3), "doublefires",  "increaseFireSeverity")
       }
       
+      # write raster to appropriate sub folder destination
+
+      ## Optimistic
+      if (str_detect(n3, "126")) {
+        writeRaster(
+          r3,
+          filename = paste0(
+            exportPath,
+            "/Optimistic/Disturbance/TreeCoverChange_",
+            n3,
+            ".tif"
+          ),
+          overwrite = TRUE
+        )
+      }
+      
+      ## Middle of the Road
+      else if (str_detect(n3, "245")) {
+        writeRaster(
+          r3,
+          filename = paste0(
+            exportPath,
+            "/MiddleOfTheRoad/Disturbance/TreeCoverChange_",
+            n3,
+            ".tif"
+          ),
+          overwrite = TRUE
+        )
+      }
+      
+      ## Pessimistic
+      else if (str_detect(n3, "370")) {
+        writeRaster(
+          r3,
+          filename = paste0(
+            exportPath,
+            "/Pessimistic/Disturbance/TreeCoverChange_",
+            n3,
+            ".tif"
+          ),
+          overwrite = TRUE
+        )
+      }
+      
+      ## Extreme
+      else if (str_detect(n3, "585")) {
+        writeRaster(
+          r3,
+          filename = paste0(
+            exportPath,
+            "/Extreme/Disturbance/TreeCoverChange_",
+            n3,
+            ".tif"
+          ),
+          overwrite = TRUE
+        )
+      }
+    }
+  }
+}
+
+
+#' Export Validation Rasters
+#' @param file : path to rds file of validation rasters
+#' @param exportPath : folder location to save outputs 
+#' @description Pulls the rasters in the given .RDS files and writes about a series of .tifs saved to specified subfolders
+exportValidationRasters <- function(file, exportPath) {
+  
+  #argument to create export path if does not already exist
+  if (!dir.exists(exportPath)) {
+    dir.create(exportPath)
+  }
+  
+  # create sub folder
+  if (!dir.exists(paste0(exportPath, "/Validation"))) {
+    dir.create(paste0(exportPath, "/Validation"))
+  }
+  
+  #read in .RDS file
+  x <- readRDS(file)
+  
+  # pull all raster bricks objects
+  r <- list_flatten(x) %>% keep( ~ is(.x, "RasterBrick"))
+  
+  # grab names to iterate over
+  n1 <- names(r)
+  
+  #iterate over raster brick to save each layer with appropriate name
+  for (i in seq_along(n1)) {
+    
+    print(paste("export",n1[i]))
+    
+    #grab first element and all layer names
+    r2 <- r[[i]]
+    n2 <- names(r2)
+    
+    #save each individual layer
+    for (j in seq_along(n2)) {
+      
+      #select layer
+      r3 <- r2[[j]]
+      
+      # write raster
+      writeRaster(
+        r3,
+        filename = paste0(
+          exportPath,
+          "/Validation/",
+          names(r3),
+          ".tif"
+        ),
+        overwrite = TRUE
+      )
     }
   }
 }
 
 
 #' Export filenames 
-#'
 #' @param exportPath : folder path of the exported datasets 
-#'
 #' @return : csv writen to folder path 
 gatherFilesNames <- function(exportPath){
-  files <- list.files(path = exportPath, pattern = ".tif")
+  files <- list.files(path = exportPath, pattern = ".tif", recursive = TRUE)
   df <- data.frame(names = files)
   write.csv(df, paste0(exportPath,"/filenames.csv"))
 }
 
 
 # implement ------------------------------------------------------------
-files <- list.files("~/Documents/kenyaAfforestation/appData",
-                    pattern = ".RDS",
-                    full.names = TRUE)
-purrr::map(.x = files, .f = exportRaster, .progress = TRUE, exportPath = exportPath)
+exportClimateRasters(file = "appData/climateChangeInputs.RDS", exportPath = "exportData/")
+exportDisturbanceRasters(file = "appData/climateManagementInputs.RDS", exportPath = "exportData/")
+exportValidationRasters(file = "appData/validationInputs_carbon.RDS", exportPath = "exportData/")
+exportValidationRasters(file = "appData/validationInputs_npp.RDS", exportPath = "exportData/")
 
-#this doesnt work for features that are stored within nested lists. doing direclty here
-file1 <- readRDS("~/Documents/kenyaAfforestation/appData/validationInputs_carbon.RDS")
-file2 <- readRDS("~/Documents/kenyaAfforestation/appData/validationInputs_npp.RDS")
-files <- c(file1$above, file1$below, file2$rasters)
-# call agian with specieis files. 
-exportRaster(file = files, exportPath = exportPath)
-
-# test new workflow
-exportClimateRasters(file = "appData/climateChangeInputs.RDS", exportPath = "test/")
-
-
-
+gatherFilesNames(exportPath = "exportData/")
